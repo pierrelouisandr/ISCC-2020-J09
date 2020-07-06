a46a5d0392a0771628c14ef6c5425d551ebf920a
pierrelouisandr <67279555+pierrelouisandr@users.noreply.github.com>
2020-07-07 00:50:40 +0200

    Jour N°9

---
+++ EX-01/fichier admin.php
@@ -0,0 +1,31 @@
+<form action= "fichier admin.php" method= "POST" enctype= "multipart/form-data">
+<input type="file" accept= "image/png, image/jpeg, image/png" name= "fichier">
+<input type="text" name = "titre">
+<input type="text" name = "description">
+<input type="submit" name = "submit">
+</form>
+
+<?php
+if ($_FILES  ['size'] > 2097152){
+    echo "Le fichier est trop volumineux";
+    $uploadOK = 0;
+}
+
+if ($_FILES  ['name']);
+$longueur = strlen($_FILES  ['name']);
+
+if ($longueur < 4){
+    echo "Le nom du fichier n'est pas assez volumineux";
+}
+
+session_start ();
+$_SESSION ['description'] = $_POST ['description'];
+$_SESSION ['titre'] = $_POST ['titre'];
+echo "<p>" . $_SESSION ['description'] . "</p>";
+echo "<p>" . $_SESSION ['titre'] . "</p>";
+
+move_uploaded_file ($_FILES ['tmp_name'],"./" .$_FILES  ['name']);
+
+$_SESSION ['image'] = $_FILES ['name'];
+
+?>
